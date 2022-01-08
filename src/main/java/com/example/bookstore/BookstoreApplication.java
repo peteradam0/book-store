5a0eb7394162243fde44ceec2d07a20fe3991276
AzureAD\PeterAdam <peter.adam@agilenetworks.tech>
2022-01-08 09:22:34 +0200

    refactoring

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -13,6 +13,7 @@ public class BookstoreApplication {
             // your Sentry project/dashboard
             options.setDsn("https://cc4e69e7e9694d9e9e33ccb7b6ff528a@o1029631.ingest.sentry.io/6138345");
             options.setRelease("senttry-demo@1.1.0+1");
+            options.setEnvironment("Development");
             options.setBeforeSend((event, hint) -> {
                 // Drop an event altogether:
                 if (event.getTag("SomeTag") != null) {
